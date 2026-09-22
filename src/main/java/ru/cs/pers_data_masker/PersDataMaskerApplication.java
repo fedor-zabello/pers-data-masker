c36df88081baf6fc39809f8a0024de20d1dcbb4a
zabellofi <zabellofi@alfastrah.ru>
2026-09-22 11:02:33 +0300

    init

---
+++ src/main/java/ru/cs/pers_data_masker/PersDataMaskerApplication.java
@@ -0,0 +1,13 @@
+package ru.cs.pers_data_masker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersDataMaskerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersDataMaskerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/cs/pers_data_masker/PersDataMaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cs.pers_data_masker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersDataMaskerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
