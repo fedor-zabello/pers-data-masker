765ad5e60f9751ac01fdf4c43b918e81fcd79be1
Николай <1986Lutik1986>
2026-09-23 15:55:02 +0300

    Правка обработки ошибок и размер тела запроса

--- src/main/java/ru/cs/pers_data_masker/api/ApiExceptionHandler.java
+++ src/main/java/ru/cs/pers_data_masker/api/ApiExceptionHandler.java
@@ -5,9 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import ru.cs.pers_data_masker.service.DemaskingNotAllowedException;
 import ru.cs.pers_data_masker.service.TooManyRequestsException;
 
@@ -31,6 +36,37 @@ public class ApiExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ApiError> handleUnreadable(HttpMessageNotReadableException e) {
         return ResponseEntity.badRequest().body(new ApiError(400, "BAD_REQUEST", "Malformed request body"));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ApiError> handleUnsupportedMediaType(HttpMediaTypeNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                .body(new ApiError(415, "UNSUPPORTED_MEDIA_TYPE",
+                        "Content-Type must be application/json"));
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    public ResponseEntity<ApiError> handleNotAcceptable(HttpMediaTypeNotAcceptableException e) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                .body(new ApiError(406, "NOT_ACCEPTABLE", "Requested media type is not acceptable"));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ApiError> handleMethodNotSupported(HttpRequestMethodNotSupportedException e) {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(new ApiError(405, "METHOD_NOT_ALLOWED", e.getMessage()));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ApiError> handleMissingParameter(MissingServletRequestParameterException e) {
+        return ResponseEntity.badRequest()
+                .body(new ApiError(400, "BAD_REQUEST", "Missing required parameter: " + e.getParameterName()));
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<ApiError> handlePayloadTooLarge(MaxUploadSizeExceededException e) {
+        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                .body(new ApiError(413, "PAYLOAD_TOO_LARGE", "Request body too large"));
     }
 
     @ExceptionHandler(DemaskingNotAllowedException.class)
